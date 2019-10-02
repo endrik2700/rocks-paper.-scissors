3bdf513aacb75aaa122a730363d1500e942f1ef4
endrik2700 <42510329+endrik2700@users.noreply.github.com>
2019-10-02 19:17:28 +0300

    Update Program.cs

--- Rock, paper, scissors/Program.cs
+++ Rock, paper, scissors/Program.cs
@@ -1,22 +1,30 @@
-﻿using System;
-
-namespace Rock__paper__scissors
+﻿namespace Rock__paper__scissors
 {
     class Program
     {
 
         public static int computerChoice;
         public static int userPick;
+        public static int userPoints =0;
+        public static int computerPoints=0;
         public static void Main(string[] args)
         {
+
             Console.WriteLine("Hello! Pick your nr from 1-3");
              userPick = int.Parse(Console.ReadLine());
             Random newRand = new Random();
             computerChoice = newRand.Next(1, 4);
-            Console.WriteLine(Comparer(userPick, computerChoice));
+            while (userPoints <=3 || computerPoints <=3) 
+            {
+                Console.WriteLine(Comparer(userPick, computerChoice));
+                if (computerPoints ==3 || userPoints ==3)
+                {
+                    break;
+                }
+            }
+            
             Console.ReadLine();
-
-
+            }
         }
         public static String Comparer(int nr1, int nr2 )
         {
@@ -51,23 +59,30 @@ namespace Rock__paper__scissors
                 pickedByUser = "scissors";
             }
              
-            if (userPick> computerChoice)
+            if (userPick ==1 & computerChoice ==2 || userPick ==2 && computerChoice==3 || userPick ==3 && computerChoice ==1)
             {
          result =(" You picked " + pickedByUser + " and won! Congrats! Computer picked " + pickedByComp);
+         userPoints +=1;
+         Console.WriteLine("Computer has " + computerPoints + " points and user has " + userPoints + " points");
+        // Console.ReadLine();
+
             }
             
-            else if (userPick <computerChoice)
+            else if (userPick == 1 &&computerChoice== 3 || userPick ==3 &&computerChoice ==2 || userPick ==2 &&computerChoice 1)
                 {
                 result = (" You picked " + pickedByUser + " and lost" + " Computer picked " + pickedByComp);
+                computerPoints +=1;
+                Console.WriteLine("Computer has " + computerPoints + " points and user has " + userPoints + " points");
+                //Console.ReadLine();
             }
              else
             {
-                result = " You picked "  + pickedByUser + " and  Computer picked " + pickedByComp + "and it's a die ";
-          
+                result = " You picked "  + pickedByUser + " and  Computer picked " + pickedByComp + " and it's a die ";
+          Console.WriteLine("Computer has " + computerPoints + " points and user has " + userPoints + " points");
+        // Console.ReadLine();
             }
             return result;
         }
             
         }
     }       
-
